e189059ff1d0ab099a12622adf677443d3b3c3e1
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-30 00:23:15 +0300

    add class hidedesign to webpage and stylesheet

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".clickable_1").click(function() {
+        $(".hidedesign").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $(".clickable_2").click(function() {
+        $(".hidedevelopment").toggle();
+    });
+});
