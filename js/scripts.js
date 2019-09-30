564a5c9881f48f447c29f9ab28b810fc8430b504
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-30 10:35:17 +0300

    Delete mailchimpAPI from javascript

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,17 @@ $(document).ready(function() {
 });
 
 //PORTFOLIO SECTION
+$(document).ready(function() {
 var portfolioIds = ['port1','port2','port3','port4','port5','port6','port7','port8'];
-    portfolioIds.forEach(function(id){
-        $(id).hover(function () {
+    portfolioIds.forEach(function(portfolioId){
+        $(portfolioId).hover(function () {
             $(id).show();
         });
-        $(id).hover(function () {
+        $(portfolioId).hover(function () {
             $(id).hide();
         });
     });
+});
 
 //SUBMIT FORM
 $(document).ready(function(){
@@ -37,32 +39,8 @@ $(document).ready(function(){
 });
 
 // RESET FORM
-$("button").click(function(event){
-    $(".message")[0].reset();
-});
-
-//MAILCHIMP API
-$(".message").bind("#submit",function(){
-    var name = $("#name").val;
-    var email= $("#email").val;
-    var message = $(".message").val;
-    var link = "https://us20.api.mailchimp.com/2.0/lists/subscribe.json?" + "apikey=ad9f836f93af6bdcba872119e1a0860c-us20id=2b641f2179" +
-    "&email[email]="+ email +
-    "&merge_vars[NAME]="+ name+
-    "&merge_vars[MESSAGE]=" +message+
-    "&send_welcome=false";
-}
-    
-$.ajax({
-    type:"POST",
-    url:link,
-    dataType:'json',
-    success: function(data){
-        var name = $("#name").val();
-    alert(name + " " + " we have received your message. Thank you for subscribing");
-    },
-    error: function(jqXHR,textStatus,errorThrown){
-        alert(errorThrown);
-    }
-});
+$(document).ready(function() {
+    $("button").click(function(event){
+        $(".message")[0].reset();
+    });
 });
